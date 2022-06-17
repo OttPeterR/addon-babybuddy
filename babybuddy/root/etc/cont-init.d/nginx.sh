5bf27514fb85baddd5a75fe63309497d922dc6db
OttPeterR <ott.peter.r@icloud.com>
2022-06-17 11:02:32 -0400

    updating execution scripts

--- babybuddy/root/etc/cont-init.d/nginx.sh
+++ babybuddy/root/etc/cont-init.d/nginx.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 
 declare ingress_port
 declare ingress_interface
@@ -11,3 +11,4 @@ ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
+
