152e9eebfae77722d77d377ef95ffb9b45fa9a05
OttPeterR <ott.peter.r@icloud.com>
2022-06-17 12:59:08 -0400

    updating nginx usage

--- babybuddy/root/etc/cont-init.d/nginx.sh
+++ babybuddy/root/etc/cont-init.d/nginx.sh
@@ -1,14 +1,11 @@
 #!/command/with-contenv bashio
 
-declare ingress_port
-declare ingress_interface
-declare ingress_entry
+declare admin_port
 
-ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-ingress_entry=$(bashio::addon.ingress_entry)
-
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
-
+# Generate Ingress configuration
+bashio::var.json \
+    interface "$(bashio::addon.ip_address)" \
+    port "^$(bashio::addon.ingress_port)" \
+    | tempio \
+        -template /etc/nginx/templates/ingress.gtpl \
+        -out /etc/nginx/servers/ingress.conf
--- babybuddy/root/etc/nginx/templates/ingress.gtpl
+++ babybuddy/root/etc/nginx/templates/ingress.gtpl
@@ -1,14 +1,14 @@
 server {
-    listen %%interface%%:%%port%% default_server;
+    listen {{ .interface }}:{{ .port }} default_server;
 
     include /etc/nginx/includes/server_params.conf;
     include /etc/nginx/includes/proxy_params.conf;
 
-    location %%ingress_entry%% {
+    location / {
         allow   172.30.32.2;
         deny    all;
 
-	proxy_pass http://backend;
-        #proxy_pass http://backend%%ingress_entry%%/;
+        proxy_pass http://backend;
     }
 }
+
